bcc07cef0d773ca9308a686c7abdd5cfa18d274c
Philipp Zakharov <philippzakharov@gmail.com>
2023-07-19 11:07:09 +0300

    setup webpack

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-import './styles/global/index.scss'
+import './styles/global/index.scss';
+
+import './styles/common/header.scss';
+
+import './styles/index_page/section_1.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,15 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 const pages = ['index'];
 
-module.exports = (env, argv) => {
-  const isDev = argv.mode === 'development';
-
-  const postCssLoader = {
-    loader: 'postcss-loader',
-    options: {
-      postcssOptions: {
-        plugins: [
-          autoprefixer({
-            overrideBrowserslist: ['last 4 major versions'],
-          }),
-        ],
-      },
-    },
-  };
-
-  const plugins = [
-    new MiniCssExtractPlugin({
-      filename: isDev ? '[name].css' : '[name].[contenthash].css',
-      chunkFilename: isDev ? '[name].css' : '[name].[contenthash].css',
-    }),
-  ].concat(
-    pages.map(
-      (page) =>
-        new HtmlWebpackPlugin({
-          inject: 'body',
-          template: `./src/${page}.html`,
-          filename: `${page}.html`,
-          chunks: [page],
-        }),
-    ),
-  );
-
-  let optimization = {
-    splitChunks: {
-      chunks: 'all',
-    },
-    minimize: true,
-    minimizer: [
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [['optipng', { optimizationLevel: 3 }]],
-          },
-        },
-      }),
-      new TerserPlugin({
-        parallel: true,
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            unsafe: true,
-            unused: true,
-          },
-          output: {
-            comments: false,
-          },
-        },
-      }),
-    ],
-  };
-
-  if (isDev) {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-    optimization = undefined;
-  }
+module.exports = (env) => {
+  const isBuild = env.WEBPACK_BUILD;
 
   return {
-    mode: argv.mode || 'development',
+    mode: isBuild ? 'development' : 'production',
     entry: pages.reduce((config, page) => {
       config[page] = `./src/${page}.ts`;
       return config;
@@ -85,8 +17,9 @@ module.exports = (env, argv) => {
     output: {
       publicPath: '/',
       path: path.resolve(__dirname, 'dist'),
-      filename: isDev ? '[name].js' : '[name].[contenthash].js',
-      clean: true,
+      filename: isBuild ? '[name].[contenthash].js' : '[name].js',
+      clean: isBuild ? true : false,
+      assetModuleFilename: 'images/[contenthash][ext]',
     },
     devServer: {
       port: 3000,
@@ -94,6 +27,10 @@ module.exports = (env, argv) => {
     },
     module: {
       rules: [
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
         {
           test: /\.ts(x)?$/,
           loader: 'ts-loader',
@@ -116,19 +53,39 @@ module.exports = (env, argv) => {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
           type: 'asset/resource',
         },
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-        },
       ],
     },
     resolve: {
       extensions: ['.ts', '.js'],
     },
-    plugins,
-    performance: {
-      hints: false,
-    },
-    optimization,
+    plugins: pages
+      .map(
+        (page) =>
+          new HtmlWebpackPlugin({
+            inject: 'body',
+            template: `./src/${page}.html`,
+            filename: `${page}.html`,
+            chunks: [page],
+          }),
+      )
+      .concat(
+        new MiniCssExtractPlugin({
+          filename: isBuild ? '[name].[contenthash].css': '[name].css',
+          chunkFilename: isBuild ? '[name].[contenthash].css': '[name].css',
+        }),
+      ),
   };
+};
+
+const postCssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    postcssOptions: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['last 4 major versions'],
+        }),
+      ],
+    },
+  },
 };
