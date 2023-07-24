8ed31bff0fe87cff0e7a06cfe923ffc995dca3d8
Philipp Zakharov <philippzakharov@gmail.com>
2023-07-24 12:30:22 +0300

    about page

---
+++ src/about.ts
@@ -0,0 +1,23 @@
+import './styles/global/index.scss';
+
+import './styles/common/header.scss';
+import './styles/common/footer.scss';
+import './styles/common/hovered_card.scss';
+import './styles/common/dropdown.scss';
+
+import './styles/about_page/section_1.scss';
+import './styles/about_page/section_2.scss';
+import './styles/about_page/section_3.scss';
+import './styles/about_page/section_4.scss';
+import './styles/about_page/section_5.scss';
+import './styles/about_page/section_6.scss';
+
+import { addDropdown } from './scripts/common/dropdown';
+import { addFooterAccordion } from './scripts/common/footer_accordion';
+import { addHeaderBurgerMenu } from './scripts/common/header_burger_menu';
+
+window.onload = () => {
+  addHeaderBurgerMenu();
+  addDropdown();
+  addFooterAccordion();
+};
